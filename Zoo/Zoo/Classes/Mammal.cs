6bed5b43da0db3e4508ae3e834ddbe2477486c96
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-12-28 23:56:44 -0800

    Made some tangible progress!

---
+++ Zoo/Zoo/Classes/Mammal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    abstract class Mammal : Animal
+    {
+        override public string SkinTexture { get => SkinTexture; set => SkinTexture = value; } = "fur";
+
+        override public bool LaysEggs { get => LaysEggs; set => LaysEggs = value; } = false;
+
+        override public bool WarmBlood { get => WarmBlood; set => WarmBlood = value; } = true;
+
+    }
+}
